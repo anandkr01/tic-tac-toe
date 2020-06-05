9d3583a7e4a6620439952b68678229d3adcaf2e4
anand kumar singh <anandkrsingh3196@gmail.com>
2020-06-05 20:19:31 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
+
 import './App.css';
+import Square from './Square';
+import {calculateWinner} from './helper'
 
 function App() {
+
+  const [value,setValue] = useState(Array(9).fill(null));
+  const [isturnx,setturnx]=useState(true)
+  const winner=calculateWinner(value);
+
+
+  
+  const clickchange=(i)=>
+  { 
+    let valuecopy=[...value];
+    
+    if (valuecopy[i]||winner)
+    {
+      
+    }
+    else if(isturnx)
+    {
+      console.log(isturnx)
+        valuecopy[i]='X';
+        setturnx(!isturnx);
+    }
+    else {
+      
+      valuecopy[i]='O';
+      setturnx(!isturnx)
+      console.log(isturnx)
+    }
+    
+    setValue(valuecopy)
+  }
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Square value={value[0]} click={()=>clickchange(0)}/>
+      <Square value={value[1]} click={()=>clickchange(1)}/>
+      <Square value={value[2]} click={()=>clickchange(2)}/>
+      <Square value={value[3]} click={()=>clickchange(3)}/>
+      <Square value={value[4]} click={()=>clickchange(4)}/>
+      <Square value={value[5]} click={()=>clickchange(5)}/>
+      <Square value={value[6]} click={()=>clickchange(6)}/>
+      <Square value={value[7]} click={()=>clickchange(7)}/>
+      <Square value={value[8]} click={()=>clickchange(8)}/>
+
     </div>
+  <p>winner : {winner? winner:null}</p>
+  
+    </>
   );
 }
 
